d000e3c4934f5b34b4c2cf70e4bfddfbe0b588da
Nirish Alakhramsing <alakhrn001@hva.nl>
2015-10-21 23:24:09 +0200

    Fix: Weapon can now be picked up.
    Removed collision boxes on weapons. Weapon wil now be removed when
    picked up and placed on players right side.

---
+++ Assets/Standard Assets/Scripts/WeaponHolderScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponHolderScript : MonoBehaviour {
+
+
+    public GameObject bazooka;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    //Puts weapon onto players right side
+    public void AquireWeapon(int weaponNumber)
+    {
+        if (weaponNumber == 0)
+        {
+            GameObject aquiredWeapon= Instantiate(bazooka, transform.position, Quaternion.identity) as GameObject;
+            
+            aquiredWeapon.transform.parent = gameObject.transform;
+            aquiredWeapon.transform.localRotation = gameObject.transform.localRotation;
+         }
+    }
+}
--- Assets/Standard Assets/Scripts/WeaponSpawnerScript.cs
+++ Assets/Standard Assets/Scripts/WeaponSpawnerScript.cs
@@ -5,49 +5,68 @@ using System.Collections.Generic;
 public class WeaponSpawnerScript : MonoBehaviour {
 
     public float speed;
+    private int weaponType;
     public bool showRocket;
     public GameObject weaponOne;
+    private WeaponHolderScript getWeaponHolderScript;
 
 	// Use this for initialization
 	void Start () {
-
         showRocket = false;
+        getWeaponHolderScript = GameObject.Find("WeaponHolder").GetComponent<WeaponHolderScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
         transform.Rotate(Vector3.up * speed * Time.deltaTime);
         SpawnWeapon();
     }
 
+    //Spawn weaponType on location
     void SpawnWeapon()
     {
         if (showRocket)
         {
-            GameObject rocket = Instantiate(weaponOne, transform.position, Quaternion.identity) as GameObject;
-            rocket.transform.parent = gameObject.transform;
-            Component.Destroy(rocket.GetComponent<BazookaScript>());
-            //foreach (Component part in rocket.transform) { 
-            Component.Destroy(rocket.GetComponentInChildren<CapsuleCollider>());
-            //}
+            GameObject bazooka = Instantiate(weaponOne, transform.position, Quaternion.identity) as GameObject;
+            bazooka.transform.parent = gameObject.transform;
+            bazooka.transform.localRotation = (gameObject.transform.localRotation);
+            Component.Destroy(bazooka.GetComponent<BazookaScript>());
+            weaponType = 0;
             showRocket = false;
         }
     }
 
-    void RemoveChild()
-    {
-        foreach(Transform rocket in gameObject.transform)
-        {
-            Destroy(rocket.gameObject);
-        }
-    }
 
+    //Remove it from spawn location and pass weapontype 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            PassWeaponType(weaponType);
             RemoveChild();
+        }
+    }
+
+    //Pass weapontype to player weaponHolderScript
+    void PassWeaponType(int weaponNumber)
+    {
+
+        if (weaponNumber!= null)
+        {
+            getWeaponHolderScript.AquireWeapon(weaponNumber);
+        }
+        else
+        {
+            Debug.Log("No weapontype passed to player");
+        }
+
+    }
+
+    void RemoveChild()
+    {
+        foreach (Transform bazooka in gameObject.transform)
+        {
+            Destroy(bazooka.gameObject);
         }
     }
 }
